6440807455c2f6c1c9e0c08d0478c2138ee3eeea
ranadheer.k.2451@gmail.com <ranadheer.k.2451@gmail.com>
2022-02-09 00:15:09 +0530

    Add project files.

---
+++ Models/EmployeeDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCApp.Models
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> employees { get; set; }
+    }
+}
